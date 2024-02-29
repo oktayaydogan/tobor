e9b55fe705b211fb24bee46797e99c326397aad8
Oktay Aydoğan <aydoganooktay@gmail.com>
2024-02-29 10:49:10 +0300

    Add server listening code

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const express = require("express");
 const app = express();
 app.get("/", (req, res) => res.send("Hello World!"));
 
+app.listen(8080, () => {
+	console.log("Example app listening at http://localhost:3000");
+});
+
 global.client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
