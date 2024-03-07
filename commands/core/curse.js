1a91598e5af65763085fde0b078897494b3c1b2e
Oktay Aydoğan <aydoganooktay@gmail.com>
2024-03-07 13:38:24 +0300

    Random curse added

--- commands/core/curse.js
+++ commands/core/curse.js
@@ -2,13 +2,47 @@ module.exports = {
 	name: "curse",
 	description: "curse it!",
 	async execute({ client, inter }) {
+		// get random member all members
+
+		const members = await inter.guild.members.fetch();
+		const member = members.random();
+
+		//if random user is bot, get another random user
+		if (member.user.bot) {
+			//return this.execute({ client, inter });
+		}
+
+		// send message and tag the user
+
 		curses = [
-			"elim çimdik sikim dimdik",
-			"arsızın götüne kazık çakmışlar, tıkırtı nerden geliyo demiş",
-			"fare sığamadığı deliğe, bir de kıçına kabak bağlayıp girmeyi denermiş",
+			"yarrağımın antifirizi",
+			"seni kibrit kutusuna koyar, sallar sallar havaya atar, yere bir parmak kala parmaklarım",
+			"mezarında etine tenezzül edecek böceğin izzeti nefsini sikeyim",
+			"orospunun çıkartıp, kerhane köşelerine attigi kansız piç.",
+			"seni bi sikerim borsa dalgalanır.",
+			"allah canımı alsada ölmüşlerini siksem.",
+			"şafak vakti sikilmiş kedi amcığı gibi durma!",
+			"annene söyle ikide bir ruj değiştirip durmasın sikim gökkuşağına döndü.",
+			"ağzına köyün köpeği işesin.",
+			"senin ağzına yüzüne sıçsam makyaj olur be!",
+			"amcuk ağızlı bok boğazlı götveren",
+			"amına götüm girsin.",
+			"ananı duvara asar amına da sımaç basarım",
+			"40 orospu bir araya gelse böyle çocuk doğuramaz..",
+			"senin azı dişine oturur ağzının tavanına sıçarım!",
+			"senin amına modem takarım, bütün dünya bağlanıp siker.",
+			"amına koyarım sıçılmamış bok.",
+			"seni bi sikerim, sebebini de soylemem, ömrün boyu bu paradoksla yasarsin.",
+			"senin ruhunu sikerim kemiklerin sızlar.",
+			"hay götüne bukalemun kaçsın da ortamla uyum sağlasın, bulup da çıkaramasınlar inşallah",
 		];
-		const m = await inter.editReply(
-			curses[Math.floor(Math.random() * curses.length)]
+
+		//remove message
+		inter.deleteReply();
+
+		// send new message
+		await inter.channel.send(
+			`<@${member.id}> ${curses[Math.floor(Math.random() * curses.length)]}`
 		);
 	},
 };
