795e35e33538112192c10ad3315c6e6ed397592b
Oktay Aydoğan <aydoganooktay@gmail.com>
2024-03-07 13:37:59 +0300

    Update app.listen port to 8080

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(8080, () => {
-	console.log("Example app listening at http://localhost:3000");
+	console.log("Example app listening at http://localhost:8080");
 });
 
 global.client = new Client({
